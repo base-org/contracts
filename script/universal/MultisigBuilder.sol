ac02c69160883a7d247fb81cc7a4aff129ec1276
Michael de Hoog <michael.dehoog@coinbase.com>
2023-06-15 09:39:11 -1000

    Move common code into base contract

--- script/universal/MultisigBuilder.sol
+++ script/universal/MultisigBuilder.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+import "./MultisigBase.sol";
+
 import { console } from "forge-std/console.sol";
 import { IMulticall3 } from "forge-std/interfaces/IMulticall3.sol";
 
-import { LibSort } from "@eth-optimism-bedrock/scripts/libraries/LibSort.sol";
-import { IGnosisSafe, Enum } from "@eth-optimism-bedrock/scripts/interfaces/IGnosisSafe.sol";
 import { EnhancedScript } from "@eth-optimism-bedrock/scripts/universal/EnhancedScript.sol";
 import { GlobalConstants } from "@eth-optimism-bedrock/scripts/universal/GlobalConstants.sol";
 
@@ -13,12 +13,7 @@ import { GlobalConstants } from "@eth-optimism-bedrock/scripts/universal/GlobalC
  * @title MultisigBuilder
  * @notice Modeled from Optimism's SafeBuilder, but using signatures instead of approvals.
  */
-abstract contract MultisigBuilder is EnhancedScript, GlobalConstants {
-    /**
-     * @notice Interface for multicall3.
-     */
-    IMulticall3 internal constant multicall = IMulticall3(MULTICALL3_ADDRESS);
-
+abstract contract MultisigBuilder is EnhancedScript, GlobalConstants, MultisigBase {
     /**
      * -----------------------------------------------------------
      * Virtual Functions
@@ -68,32 +63,6 @@ abstract contract MultisigBuilder is EnhancedScript, GlobalConstants {
         return success;
     }
 
-    function _getTransactionHash(address _safe, bytes memory _data) internal view returns (bytes32) {
-        return keccak256(_encodeTransactionData(_safe, _data));
-    }
-
-    function _encodeTransactionData(address _safe, bytes memory _data) internal view returns (bytes memory) {
-        // Ensure that the required contracts exist
-        require(address(multicall).code.length > 0, "multicall3 not deployed");
-        require(_safe.code.length > 0, "no code at safe address");
-
-        IGnosisSafe safe = IGnosisSafe(payable(_safe));
-        uint256 nonce = safe.nonce();
-
-        return safe.encodeTransactionData({
-            to: address(multicall),
-            value: 0,
-            data: _data,
-            operation: Enum.Operation.DelegateCall,
-            safeTxGas: 0,
-            baseGas: 0,
-            gasPrice: 0,
-            gasToken: address(0),
-            refundReceiver: address(0),
-            _nonce: nonce
-        });
-    }
-
     function _generateExecuteCall(address _target) internal returns (IMulticall3.Call3 memory) {
         bytes memory data = buildCalldata(_target);
         return IMulticall3.Call3({
@@ -101,81 +70,5 @@ abstract contract MultisigBuilder is EnhancedScript, GlobalConstants {
             allowFailure: false,
             callData: data
         });
-    }
-
-    function _printDataToSign(address _safe, IMulticall3.Call3 memory _call) internal {
-        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
-        calls[0] = _call;
-        bytes memory data = abi.encodeCall(IMulticall3.aggregate3, (calls));
-
-        bytes memory txData = _encodeTransactionData(_safe, data);
-        console.log("Data to sign:");
-        console.log("vvvvvvvv");
-        console.logBytes(txData);
-        console.log("^^^^^^^^");
-    }
-
-    function _executeTransaction(address _safe, IMulticall3.Call3 memory _call, bytes memory _signatures) internal returns (bool) {
-        IGnosisSafe safe = IGnosisSafe(payable(_safe));
-        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
-        calls[0] = _call;
-        bytes memory data = abi.encodeCall(IMulticall3.aggregate3, (calls));
-        bytes32 hash = _getTransactionHash(_safe, data);
-
-        uint8 v = 1;
-        bytes32 s = bytes32(0);
-        bytes32 r = bytes32(uint256(uint160(msg.sender)));
-        _signatures = bytes.concat(_signatures, abi.encodePacked(r, s, v));
-
-        return safe.execTransaction({
-            to: address(multicall),
-            value: 0,
-            data: data,
-            operation: Enum.Operation.DelegateCall,
-            safeTxGas: 0,
-            baseGas: 0,
-            gasPrice: 0,
-            gasToken: address(0),
-            refundReceiver: payable(address(0)),
-            signatures: sortSignatures(_signatures, hash)
-        });
-    }
-
-    function sortSignatures(bytes memory _signatures, bytes32 dataHash) internal returns (bytes memory) {
-        bytes memory sorted;
-        uint256 count = uint256(_signatures.length / 0x41);
-        uint256[] memory addressesAndIndexes = new uint256[](count);
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-        for (uint256 i; i < count; i++) {
-            (v, r, s) = signatureSplit(_signatures, i);
-            address owner;
-            if (v <= 1) {
-                owner = address(uint160(uint256(r)));
-            } else if (v > 30) {
-                owner = ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", dataHash)), v - 4, r, s);
-            } else {
-                owner = ecrecover(dataHash, v, r, s);
-            }
-            addressesAndIndexes[i] = uint256(uint256(uint160(owner)) << 0x60 | i); // address in first 160 bits, index in second 96 bits
-        }
-        LibSort.sort(addressesAndIndexes);
-        for (uint256 i; i < count; i++) {
-            uint256 index = addressesAndIndexes[i] & 0xffffffff;
-            (v, r, s) = signatureSplit(_signatures, index);
-            sorted = bytes.concat(sorted, abi.encodePacked(r, s, v));
-        }
-        return sorted;
-    }
-
-    // see https://github.com/safe-global/safe-contracts/blob/1ed486bb148fe40c26be58d1b517cec163980027/contracts/common/SignatureDecoder.sol
-    function signatureSplit(bytes memory signatures, uint256 pos) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
-        assembly {
-            let signaturePos := mul(0x41, pos)
-            r := mload(add(signatures, add(signaturePos, 0x20)))
-            s := mload(add(signatures, add(signaturePos, 0x40)))
-            v := and(mload(add(signatures, add(signaturePos, 0x41))), 0xff)
-        }
     }
 }
--- script/universal/NestedMultisigBuilder.sol
+++ script/universal/NestedMultisigBuilder.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
+import "./MultisigBase.sol";
+
 import { console } from "forge-std/console.sol";
 import { IMulticall3 } from "forge-std/interfaces/IMulticall3.sol";
 
-import { LibSort } from "@eth-optimism-bedrock/scripts/libraries/LibSort.sol";
 import { IGnosisSafe, Enum } from "@eth-optimism-bedrock/scripts/interfaces/IGnosisSafe.sol";
 import { EnhancedScript } from "@eth-optimism-bedrock/scripts/universal/EnhancedScript.sol";
 import { GlobalConstants } from "@eth-optimism-bedrock/scripts/universal/GlobalConstants.sol";
@@ -13,12 +14,7 @@ import { GlobalConstants } from "@eth-optimism-bedrock/scripts/universal/GlobalC
  * @title NestedMultisigBuilder
  * @notice Modeled from Optimism's SafeBuilder, but built for nested safes.
  */
-abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants {
-    /**
-     * @notice Interface for multicall3.
-     */
-    IMulticall3 internal constant multicall = IMulticall3(MULTICALL3_ADDRESS);
-
+abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants, MultisigBase {
     /**
      * -----------------------------------------------------------
      * Virtual Functions
@@ -139,32 +135,6 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants {
         return success;
     }
 
-    function _getTransactionHash(address _safe, bytes memory _data) internal view returns (bytes32) {
-        return keccak256(_encodeTransactionData(_safe, _data));
-    }
-
-    function _encodeTransactionData(address _safe, bytes memory _data) internal view returns (bytes memory) {
-        // Ensure that the required contracts exist
-        require(address(multicall).code.length > 0, "multicall3 not deployed");
-        require(_safe.code.length > 0, "no code at safe address");
-
-        IGnosisSafe safe = IGnosisSafe(payable(_safe));
-        uint256 nonce = safe.nonce();
-
-        return safe.encodeTransactionData({
-            to: address(multicall),
-            value: 0,
-            data: _data,
-            operation: Enum.Operation.DelegateCall,
-            safeTxGas: 0,
-            baseGas: 0,
-            gasPrice: 0,
-            gasToken: address(0),
-            refundReceiver: address(0),
-            _nonce: nonce
-        });
-    }
-
     function _generateApproveCall(address _safe, address _nestedSafe, address _target) internal returns (IMulticall3.Call3 memory) {
         IGnosisSafe safe = IGnosisSafe(payable(_safe));
         IGnosisSafe nestedSafe = IGnosisSafe(payable(_nestedSafe));
@@ -212,93 +182,5 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants {
                 )
             )
         });
-    }
-
-    function _printDataToSign(address _safe, IMulticall3.Call3 memory _call) internal {
-        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
-        calls[0] = _call;
-        bytes memory data = abi.encodeCall(IMulticall3.aggregate3, (calls));
-
-        bytes memory txData = _encodeTransactionData(_safe, data);
-        console.log("Data to sign:");
-        console.log("vvvvvvvv");
-        console.logBytes(txData);
-        console.log("^^^^^^^^");
-    }
-
-    function _executeTransaction(address _safe, IMulticall3.Call3 memory _call, bytes memory _signatures) internal returns (bool) {
-        IGnosisSafe safe = IGnosisSafe(payable(_safe));
-        IMulticall3.Call3[] memory calls = new IMulticall3.Call3[](1);
-        calls[0] = _call;
-        bytes memory data = abi.encodeCall(IMulticall3.aggregate3, (calls));
-        bytes32 hash = _getTransactionHash(_safe, data);
-
-        uint8 v = 1;
-        bytes32 s = bytes32(0);
-        bytes32 r = bytes32(uint256(uint160(msg.sender)));
-        _signatures = bytes.concat(_signatures, abi.encodePacked(r, s, v));
-
-        return safe.execTransaction({
-            to: address(multicall),
-            value: 0,
-            data: data,
-            operation: Enum.Operation.DelegateCall,
-            safeTxGas: 0,
-            baseGas: 0,
-            gasPrice: 0,
-            gasToken: address(0),
-            refundReceiver: payable(address(0)),
-            signatures: sortSignatures(_signatures, hash)
-        });
-    }
-
-    function sortSignatures(bytes memory _signatures, bytes32 dataHash) internal returns (bytes memory) {
-        bytes memory sorted;
-        uint256 count = uint256(_signatures.length / 0x41);
-        uint256[] memory addressesAndIndexes = new uint256[](count);
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-        for (uint256 i; i < count; i++) {
-            (v, r, s) = signatureSplit(_signatures, i);
-            address owner;
-            if (v <= 1) {
-                owner = address(uint160(uint256(r)));
-            } else if (v > 30) {
-                owner = ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", dataHash)), v - 4, r, s);
-            } else {
-                owner = ecrecover(dataHash, v, r, s);
-            }
-            addressesAndIndexes[i] = uint256(uint256(uint160(owner)) << 0x60 | i); // address in first 160 bits, index in second 96 bits
-        }
-        LibSort.sort(addressesAndIndexes);
-        for (uint256 i; i < count; i++) {
-            uint256 index = addressesAndIndexes[i] & 0xffffffff;
-            (v, r, s) = signatureSplit(_signatures, index);
-            sorted = bytes.concat(sorted, abi.encodePacked(r, s, v));
-        }
-        return sorted;
-    }
-
-    function addressSignatures(address[] memory _addresses) internal view returns (bytes memory) {
-        LibSort.sort(_addresses);
-        bytes memory signatures;
-        uint8 v = 1;
-        bytes32 s = bytes32(0);
-        for (uint256 i; i < _addresses.length; i++) {
-            bytes32 r = bytes32(uint256(uint160(_addresses[i])));
-            signatures = bytes.concat(signatures, abi.encodePacked(r, s, v));
-        }
-        return signatures;
-    }
-
-    // see https://github.com/safe-global/safe-contracts/blob/1ed486bb148fe40c26be58d1b517cec163980027/contracts/common/SignatureDecoder.sol
-    function signatureSplit(bytes memory signatures, uint256 pos) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
-        assembly {
-            let signaturePos := mul(0x41, pos)
-            r := mload(add(signatures, add(signaturePos, 0x20)))
-            s := mload(add(signatures, add(signaturePos, 0x40)))
-            v := and(mload(add(signatures, add(signaturePos, 0x41))), 0xff)
-        }
     }
 }
