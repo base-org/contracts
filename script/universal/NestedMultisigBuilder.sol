e1c66093af049cec1d79f54a36b6d935c0e58ad8
anikaraghu <anika.raghuvanshi@gmail.com>
2023-06-23 01:13:04 +0100

    Fix nested multisig (#23)

--- script/universal/NestedMultisigBuilder.sol
+++ script/universal/NestedMultisigBuilder.sol
@@ -64,9 +64,9 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants, Mult
         - <s2> => send signature to <s3>
         - <s4> => send signature to <s5>
      */
-    function signApproval(address _safe) public returns (bool) {
-        IMulticall3.Call3 memory call = _generateApproveCall(_safe);
-        _printDataToSign(_safe, toArray(call));
+    function signApproval(address _signerSafe) public returns (bool) {
+        IMulticall3.Call3 memory call = _generateApproveCall();
+        _printDataToSign(_signerSafe, toArray(call));
         return true;
     }
 
@@ -90,10 +90,10 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants, Mult
         - <s3> => using signatures from <s1>, <s2>
         - <s5> => using signature from <s4>
      */
-    function runApproval(address _safe, bytes memory _signatures) public returns (bool) {
+    function runApproval(address _signerSafe, bytes memory _signatures) public returns (bool) {
         vm.startBroadcast();
-        IMulticall3.Call3 memory call = _generateApproveCall(_safe);
-        return _executeTransaction(_safe, toArray(call), _signatures);
+        IMulticall3.Call3 memory call = _generateApproveCall();
+        return _executeTransaction(_signerSafe, toArray(call), _signatures);
     }
 
     /**
@@ -116,9 +116,9 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants, Mult
         - <s7> => send signature to <s9>
         - <s8> => send signature to <s9>
      */
-    function signTransaction(address _safe) public returns (bool) {
-        IMulticall3.Call3 memory call = _generateExecuteCall(_safe);
-        _printDataToSign(_safe, toArray(call));
+    function signTransaction(address _signerSafe) public returns (bool) {
+        IMulticall3.Call3 memory call = _generateExecuteCall(_signerSafe);
+        _printDataToSign(_signerSafe, toArray(call));
         return true;
     }
 
@@ -140,10 +140,10 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants, Mult
      * The following signer should run this function:
         - <s9> => using signatures from <s7>, <s8>
      */
-    function runTransaction(address _safe, bytes memory _signatures) public returns (bool) {
+    function runTransaction(address _signerSafe, bytes memory _signatures) public returns (bool) {
         vm.startBroadcast();
-        IMulticall3.Call3 memory call = _generateExecuteCall(_safe);
-        bool success = _executeTransaction(_safe, toArray(call), _signatures);
+        IMulticall3.Call3 memory call = _generateExecuteCall(_signerSafe);
+        bool success = _executeTransaction(_signerSafe, toArray(call), _signatures);
         if (success) _postCheck();
         return success;
     }
@@ -153,27 +153,25 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants, Mult
         return abi.encodeCall(IMulticall3.aggregate3, (calls));
     }
 
-    function _generateApproveCall(address _safe) internal returns (IMulticall3.Call3 memory) {
-        IGnosisSafe safe = IGnosisSafe(payable(_safe));
-        address ownerSafe = _ownerSafe();
-        IGnosisSafe nestedSafe = IGnosisSafe(payable(ownerSafe));
+    function _generateApproveCall() internal returns (IMulticall3.Call3 memory) {
+        address ownerSafeAddress = _ownerSafe();
+        IGnosisSafe ownerSafe = IGnosisSafe(payable(ownerSafeAddress));
         bytes memory nestedData = _buildCalldata();
-        bytes32 nestedHash = _getTransactionHash(ownerSafe, nestedData);
+        bytes32 nestedHash = _getTransactionHash(ownerSafeAddress, nestedData);
         console.log("Nested hash:");
         console.logBytes32(nestedHash);
 
         return IMulticall3.Call3({
-            target: ownerSafe,
+            target: ownerSafeAddress,
             allowFailure: false,
             callData: abi.encodeCall(
-                nestedSafe.approveHash,
+                ownerSafe.approveHash,
                 (nestedHash)
             )
         });
     }
 
-    function _generateExecuteCall(address _safe) internal returns (IMulticall3.Call3 memory) {
-        IGnosisSafe safe = IGnosisSafe(payable(_safe));
+    function _generateExecuteCall(address _signerSafe) internal returns (IMulticall3.Call3 memory) {
         address ownerSafe = _ownerSafe();
         IGnosisSafe nestedSafe = IGnosisSafe(payable(ownerSafe));
         bytes memory nestedData = _buildCalldata();
@@ -181,12 +179,12 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants, Mult
 
         // get a list of owners that have approved this transaction
         uint256 threshold = nestedSafe.getThreshold();
-        address[] memory owners = safe.getOwners();
+        address[] memory owners = nestedSafe.getOwners();
         address[] memory approvals = new address[](threshold);
         uint256 approvalIndex;
         for (uint256 i; i < owners.length; i++) {
             address owner = owners[i];
-            uint256 approved = safe.approvedHashes(owner, nestedHash);
+            uint256 approved = nestedSafe.approvedHashes(owner, nestedHash);
             if (approved == 1) {
                 approvals[approvalIndex] = owner;
                 approvalIndex++;
@@ -196,7 +194,7 @@ abstract contract NestedMultisigBuilder is EnhancedScript, GlobalConstants, Mult
             }
         }
         require(approvalIndex == threshold - 1, "not enough approvals");
-        approvals[approvalIndex] = _safe; // last approver is the current user
+        approvals[approvalIndex] = _signerSafe; // last approver is the current user
 
         return IMulticall3.Call3({
             target: ownerSafe,
