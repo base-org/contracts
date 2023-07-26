85030eb77acf6dac9e48cbe458f05d322611b2c7
dapperscene6 <david.knott@coinbase.com>
2023-07-26 14:48:42 -0400

    Check-in the BalanceTracker contract (#24)
    * check-in BalanceTracker contract
    
    * implement changes based on PR comments

---
+++ test/revenue-share/mocks/ReenterProcessFees.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import { BalanceTracker } from "src/revenue-share/BalanceTracker.sol";
+
+contract ReenterProcessFees {
+    receive() external payable {
+        BalanceTracker(payable(msg.sender)).processFees();
+    }
+}
