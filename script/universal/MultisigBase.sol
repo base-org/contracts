54dd998fc09719c982206ca077d65bf1538d2aff
Felipe Andrade <felipe@oplabs.co>
2023-11-20 12:26:49 -0800

    fix: safe_nonce env shouldnt be required

--- script/universal/MultisigBase.sol
+++ script/universal/MultisigBase.sol
@@ -28,7 +28,7 @@ abstract contract MultisigBase is Simulator {
         uint256 nonce = safe.nonce();
         console.log("Safe current nonce:", nonce);
 
-        if (bytes(vm.envString("SAFE_NONCE")).length > 0) {
+        if (bytes(vm.envOr("SAFE_NONCE", "")).length > 0) {
             nonce = vm.envUint("SAFE_NONCE");
             console.log("Creating transaction with nonce:", nonce);
         }
