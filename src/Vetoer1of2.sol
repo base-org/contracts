abc9fa624ffbd79d2c71446ad1f05a511d2f7fe5
Baptiste Oueriagli <baptiste.oueriagli@coinbase.com>
2024-04-19 08:36:42 +0000

    chore(vetoer): deploy DelayedVetoable in constructor

--- src/Vetoer1of2.sol
+++ src/Vetoer1of2.sol
@@ -2,73 +2,80 @@
 pragma solidity 0.8.15;
 
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {DelayedVetoable} from "@eth-optimism-bedrock/src/L1/DelayedVetoable.sol";
 
-/**
- * @title Vetoer1of2
- * @dev This contract serves the role of the Vetoer, defined in DelayedVetoable.sol:
- * https://github.com/ethereum-optimism/optimism/blob/d72fb46daf3a6831cb01a78931f8c6e0d52ae243/packages/contracts-bedrock/src/L1/DelayedVetoable.sol
- * It enforces a simple 1 of 2 design, where neither party can remove the other's
- * permissions to execute a Veto call.
- */
+/// @title Vetoer1of2
+///
+/// @dev This contract serves the role of the Vetoer, defined in DelayedVetoable.sol:
+///      https://github.com/ethereum-optimism/optimism/blob/d72fb46daf3a6831cb01a78931f8c6e0d52ae243/packages/contracts-bedrock/src/L1/DelayedVetoable.sol
+///      It enforces a simple 1 of 2 design, where neither party can remove the other's
+///      permissions to execute a Veto call.
+///
 contract Vetoer1of2 {
     using Address for address;
 
-    /*//////////////////////////////////////////////////////////////
-                            CONSTANTS
-    //////////////////////////////////////////////////////////////*/
-    /**
-     * @dev The address of Optimism's signer (likely a multisig)
-     */
+    /////////////////////////////////////////////////////////////
+    //                        CONSTANTS                        //
+    /////////////////////////////////////////////////////////////
+
+    /// @notice The address of Optimism's signer (likely a multisig)
     address public immutable opSigner;
 
-    /**
-     * @dev The address of counter party's signer (likely a multisig)
-     */
+    /// @notice The address of counter party's signer (likely a multisig)
     address public immutable otherSigner;
 
-    /**
-     * @dev The address of the L2OutputOracleProxy contract.
-     */
+    /// @notice The address of the L2OutputOracleProxy contract.
     address public immutable delayedVetoable;
 
-    /*//////////////////////////////////////////////////////////////
-                            EVENTS
-    //////////////////////////////////////////////////////////////*/
-    /**
-     * @dev Emitted when a Veto call is made by a signer.
-     * @param caller The signer making the call.
-     * @param data The data of the call being made.
-     * @param result The result of the call being made.
-     */
+    //////////////////////////////////////////////////////////////
+    //                        EVENTS                            //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Emitted when a Veto call is made by a signer.
+    ///
+    /// @param caller The signer making the call.
+    /// @param data The data of the call being made.
+    /// @param result The result of the call being made.
     event VetoCallExecuted(address indexed caller, bytes data, bytes result);
 
-    /*//////////////////////////////////////////////////////////////
-                            Constructor
-    //////////////////////////////////////////////////////////////*/
-    /**
-     * @dev Constructor to set the values of the constants.
-     * @param opSigner_ Address of Optimism signer.
-     * @param otherSigner_ Address of counter party signer.
-     * @param delayedVetoable_ Address of the DelayedVetoable contract.
-     */
-    constructor(address opSigner_, address otherSigner_, address delayedVetoable_) {
+    //////////////////////////////////////////////////////////////
+    //                        Constructor                       //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Constructor to set the values of the constants.
+    ///
+    /// @dev The `DelayedVetoable` contract is deployed in this constructor to easily establish
+    ///      the link between both contracts.
+    ///
+    /// @param opSigner_ Address of Optimism signer.
+    /// @param otherSigner_ Address of counter party signer.
+    /// @param initiator Address of the initiator.
+    /// @param target Address of the target.
+    /// @param operatingDelay Time to delay when the system is operational.
+    constructor(address opSigner_, address otherSigner_, address initiator, address target, uint256 operatingDelay) {
         require(opSigner_ != address(0), "Vetoer1of2: opSigner cannot be zero address");
         require(otherSigner_ != address(0), "Vetoer1of2: otherSigner cannot be zero address");
-        require(delayedVetoable_.isContract(), "Vetoer1of2: delayedVetoable must be a contract");
 
         opSigner = opSigner_;
         otherSigner = otherSigner_;
-        delayedVetoable = delayedVetoable_;
+
+        delayedVetoable = address(
+            new DelayedVetoable({
+                vetoer_: address(this),
+                initiator_: initiator,
+                target_: target,
+                operatingDelay_: operatingDelay
+            })
+        );
     }
 
-    /*//////////////////////////////////////////////////////////////
-                        External Functions
-    //////////////////////////////////////////////////////////////*/
-    /**
-     * @dev Executes a call as the Vetoer (must be called by
-     * Optimism or counter party signer).
-     * @param data Data for function call.
-     */
+    //////////////////////////////////////////////////////////////
+    //                    External Functions                    //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Executes a call as the Vetoer (must be called by Optimism or counter party signer).
+    ///
+    /// @param data Data for function call.
     function execute(bytes memory data) external {
         require(
             msg.sender == otherSigner || msg.sender == opSigner, "Vetoer1of2: must be an approved signer to execute"
