a0f86fcf67e61f08ba7d7a8b26256963379b8a60
xenoliss <baptiste.oue@proton.me>
2024-04-22 22:37:30 +0200

    Remove the `delayedVetoable` param from the `Vetoer1of2` constructor  (#76)
    * chore(vetoer): align with internal solidity style guide
    
    * chore(vetoer): provide delayedVetoable address when calling execute
    
    * Revert "chore(vetoer): provide delayedVetoable address when calling execute"
    
    This reverts commit 0a35c26f27cdcecd040086dabf7ba8b3cecee5ba.
    
    * chore(vetoer): deploy DelayedVetoable in constructor
    
    * chore(vetoer): replace generic 'execute()' by 'veto()'
    
    * typo(vetoer): L2OutputOracle -> DelayedVetoable typo
    
    * chore(vetoer): add custom errors & hardcode 14 days delay

--- src/Vetoer1of2.sol
+++ src/Vetoer1of2.sol
@@ -1,93 +1,128 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.15;
 
-import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {DelayedVetoable} from "@eth-optimism-bedrock/src/L1/DelayedVetoable.sol";
 
-/**
- * @title Vetoer1of2
- * @dev This contract serves the role of the Vetoer, defined in DelayedVetoable.sol:
- * https://github.com/ethereum-optimism/optimism/blob/d72fb46daf3a6831cb01a78931f8c6e0d52ae243/packages/contracts-bedrock/src/L1/DelayedVetoable.sol
- * It enforces a simple 1 of 2 design, where neither party can remove the other's
- * permissions to execute a Veto call.
- */
+/// @title Vetoer1of2
+///
+/// @dev This contract serves the role of the Vetoer, defined in DelayedVetoable.sol:
+///      https://github.com/ethereum-optimism/optimism/blob/d72fb46daf3a6831cb01a78931f8c6e0d52ae243/packages/contracts-bedrock/src/L1/DelayedVetoable.sol
+///      It enforces a simple 1 of 2 design, where neither party can remove the other's
+///      permissions to execute a Veto call.
+///
 contract Vetoer1of2 {
     using Address for address;
 
-    /*//////////////////////////////////////////////////////////////
-                            CONSTANTS
-    //////////////////////////////////////////////////////////////*/
-    /**
-     * @dev The address of Optimism's signer (likely a multisig)
-     */
-    address public immutable OP_SIGNER;
+    /////////////////////////////////////////////////////////////
+    //                        CONSTANTS                        //
+    /////////////////////////////////////////////////////////////
 
-    /**
-     * @dev The address of counter party's signer (likely a multisig)
-     */
-    address public immutable OTHER_SIGNER;
+    /// @notice The address of Optimism's signer (likely a multisig)
+    address public immutable opSigner;
 
-    /**
-     * @dev The address of the L2OutputOracleProxy contract.
-     */
-    address public immutable DELAYED_VETOABLE;
+    /// @notice The address of counter party's signer (likely a multisig)
+    address public immutable otherSigner;
 
-    /*//////////////////////////////////////////////////////////////
-                            EVENTS
-    //////////////////////////////////////////////////////////////*/
-    /**
-     * @dev Emitted when a Veto call is made by a signer.
-     * @param _caller The signer making the call.
-     * @param _data The data of the call being made.
-     * @param _result The result of the call being made.
-     */
-    event VetoCallExecuted(
-        address indexed _caller,
-        bytes _data,
-        bytes _result
-    );
+    /// @notice The address of the DelayedVetoable contract.
+    address public immutable delayedVetoable;
 
-    /*//////////////////////////////////////////////////////////////
-                            Constructor
-    //////////////////////////////////////////////////////////////*/
-    /**
-     * @dev Constructor to set the values of the constants.
-     * @param _opSigner Address of Optimism signer.
-     * @param _otherSigner Address of counter party signer.
-     * @param _delayedVetoable Address of the DelayedVetoable contract.
-     */
-    constructor(address _opSigner, address _otherSigner, address _delayedVetoable) {
-        require(_opSigner != address(0), "Vetoer1of2: opSigner cannot be zero address");
-        require(_otherSigner != address(0), "Vetoer1of2: otherSigner cannot be zero address");
-        require(
-            _delayedVetoable.isContract(),
-            "Vetoer1of2: delayedVetoable must be a contract"
+    //////////////////////////////////////////////////////////////
+    //                        EVENTS                            //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Emitted when a Veto call is made by a signer.
+    ///
+    /// @param caller The signer making the call.
+    /// @param result The result of the call being made.
+    event VetoCallExecuted(address indexed caller, bytes result);
+
+    //////////////////////////////////////////////////////////////
+    //                        ERRORS                            //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Thrown at deployment if `opSigner` is the zero address.
+    error OpSignerCantBeZeroAddress();
+
+    /// @notice Thrown at deployment if `otherSigner` is the zero address.
+    error OtherSignerCantBeZeroAddress();
+
+    /// @notice Thrown at deployment if `initiator` is the zero address.
+    error InitiatorCantBeZeroAddress();
+
+    /// @notice Thrown at deployment if `target` is the zero address.
+    error TargetCantBeZeroAddress();
+
+    /// @notice Thrown when calling 'veto()' from an unhautorized signer.
+    error SenderIsNotWhitelistedSigner();
+
+    //////////////////////////////////////////////////////////////
+    //                        Constructor                       //
+    //////////////////////////////////////////////////////////////
+
+    /// @notice Constructor initializing the immutable variables and deploying the `DelayedVetoable`
+    ///         contract.
+    ///
+    /// @dev The `DelayedVetoable` contract is deployed in this constructor to easily establish
+    ///      the link between both contracts.
+    ///
+    /// @custom:reverts OpSignerCantBeZeroAddress() if `opSigner_` is the zero address.
+    /// @custom:reverts OtherSignerCantBeZeroAddress() if `otherSigner_` is the zero address.
+    /// @custom:reverts InitiatorCantBeZeroAddress() if `initiator` is the zero address.
+    /// @custom:reverts TargetCantBeZeroAddress() if `target` is the zero address.
+    ///
+    /// @param opSigner_ Address of Optimism signer.
+    /// @param otherSigner_ Address of counter party signer.
+    /// @param initiator Address of the initiator.
+    /// @param target Address of the target.
+    constructor(address opSigner_, address otherSigner_, address initiator, address target) {
+        if (opSigner_ == address(0)) {
+            revert OpSignerCantBeZeroAddress();
+        }
+
+        if (otherSigner_ == address(0)) {
+            revert OtherSignerCantBeZeroAddress();
+        }
+
+        if (initiator == address(0)) {
+            revert InitiatorCantBeZeroAddress();
+        }
+
+        if (target == address(0)) {
+            revert TargetCantBeZeroAddress();
+        }
+
+        opSigner = opSigner_;
+        otherSigner = otherSigner_;
+
+        delayedVetoable = address(
+            new DelayedVetoable({
+                vetoer_: address(this),
+                initiator_: initiator,
+                target_: target,
+                operatingDelay_: 14 days
+            })
         );
-
-        OP_SIGNER = _opSigner;
-        OTHER_SIGNER = _otherSigner;
-        DELAYED_VETOABLE = _delayedVetoable;
     }
 
-    /*//////////////////////////////////////////////////////////////
-                        External Functions
-    //////////////////////////////////////////////////////////////*/
-    /**
-     * @dev Executes a call as the Vetoer (must be called by
-     * Optimism or counter party signer).
-     * @param _data Data for function call.
-     */
-    function execute(bytes memory _data) external {
-        require(
-            msg.sender == OTHER_SIGNER || msg.sender == OP_SIGNER,
-            "Vetoer1of2: must be an approved signer to execute"
-        );
+    //////////////////////////////////////////////////////////////
+    //                    External Functions                    //
+    //////////////////////////////////////////////////////////////
 
-        bytes memory result = Address.functionCall(
-            DELAYED_VETOABLE,
-            _data,
-            "Vetoer1of2: failed to execute"
-        );
+    /// @notice Passthrough for either signer to execute a veto on the `DelayedVetoable` contract.
+    ///
+    /// @custom:reverts SenderIsNotWhitelistedSigner() if not called by `opSigner` or `otherSigner`.
+    function veto() external {
+        if (msg.sender != otherSigner && msg.sender != opSigner) {
+            revert SenderIsNotWhitelistedSigner();
+        }
 
-        emit VetoCallExecuted(msg.sender, _data, result);
+        bytes memory result = Address.functionCall({
+            target: delayedVetoable,
+            data: msg.data,
+            errorMessage: "Vetoer1of2: failed to execute"
+        });
+
+        emit VetoCallExecuted({caller: msg.sender, result: result});
     }
 }
