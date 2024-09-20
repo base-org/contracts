6df77b49e16c2da29d1530b624eb6df4ff00cb2e
Brian Bland <brian.t.bland@gmail.com>
2024-09-20 15:50:33 -0700

    Add Upgrade and Rollback GasLimit scripts (#92)

---
+++ script/deploy/l1/UpgradeGasLimit.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import './SetGasLimitBuilder.sol';
+
+contract UpgradeGasLimit is SetGasLimitBuilder {
+    function _fromGasLimit() internal view override returns (uint64) {
+        return uint64(vm.envUint("OLD_GAS_LIMIT"));
+    }
+
+    function _toGasLimit() internal view override returns (uint64) {
+        return uint64(vm.envUint("NEW_GAS_LIMIT"));
+    }
+
+    function _nonceOffset() internal pure override returns (uint64) {
+        return 0;
+    }
+}
