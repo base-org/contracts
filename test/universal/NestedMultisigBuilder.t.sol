488e7a648e3220633cc23bfc7ec59b68f10812a8
Michael de Hoog <michael.dehoog@coinbase.com>
2024-10-28 15:20:26 -1000

    Remove IGnosisSafe interface copy (#102)

--- test/universal/NestedMultisigBuilder.t.sol
+++ test/universal/NestedMultisigBuilder.t.sol
@@ -7,7 +7,7 @@ import {console} from "forge-std/console.sol";
 import {IMulticall3} from "forge-std/interfaces/IMulticall3.sol";
 import {Preinstalls} from "@eth-optimism-bedrock/src/libraries/Preinstalls.sol";
 import {NestedMultisigBuilder} from "../../script/universal/NestedMultisigBuilder.sol";
-import {IGnosisSafe} from "../../script/universal/IGnosisSafe.sol";
+import {IGnosisSafe} from "@eth-optimism-bedrock/scripts/interfaces/IGnosisSafe.sol";
 import {Counter} from "./Counter.sol";
 
 contract NestedMultisigBuilderTest is Test, NestedMultisigBuilder {
