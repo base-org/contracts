12a4c94e15b808aa165ccd004e1d2ca3229ac60f
savvasmoke <136869149+savvar9991@users.noreply.github.com>
2024-11-27 16:01:51 +1100

    type MultisigBuilder.sol
    fix

--- script/universal/MultisigBuilder.sol
+++ script/universal/MultisigBuilder.sol
@@ -146,7 +146,7 @@ abstract contract MultisigBuilder is MultisigBase {
 
         Simulation.StateOverride[] memory overrides = _overrides(_safe);
 
-        bytes memory txData = _execTransationCalldata(_safe, data, Signatures.genPrevalidatedSignature(msg.sender));
+        bytes memory txData = _execTransactionCalldata(_safe, data, Signatures.genPrevalidatedSignature(msg.sender));
         Simulation.logSimulationLink({_to: _safe, _data: txData, _from: msg.sender, _overrides: overrides});
 
         // Forge simulation of the data logged in the link. If the simulation fails
